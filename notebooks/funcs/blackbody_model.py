7dab469429517b4a038d861c56b897205e7a89b4
aaronmaas <aaron.wehlmann@gmail.com>
2022-01-25 13:23:57 +0100

    Working on TRAPPIST-1 emcee notebook, created plotting functions

--- notebooks/funcs/blackbody_model.py
+++ notebooks/funcs/blackbody_model.py
@@ -1,6 +1,33 @@
 import numpy as np
 import scipy.constants as c
 
+def brightness_mod_continous(wavelength, T_flare, a, fluxdensity_tot, T_star, R_star, dist_star, model = "blackbody"):
+    pre_factor = 1/(dist_star**2) * 2*c.h*c.c**2 /(wavelength**5) * np.pi
+    flare_contribution = (a*R_star)**2/(np.exp((c.h * c.c)/(T_flare*c.k*wavelength)) -1)
+    if model == "blackbody": 
+        star_contribution = -(a*R_star)**2/(np.exp((c.h * c.c)/(T_star*c.k*wavelength)) -1)
+    else: 
+        star_contribution = -(a*R_star)**2* fluxdensity_tot * 1e7
+    return pre_factor * (flare_contribution + star_contribution) 
+
+def _brightness_mod(wavelength_tot, limit, T_flare, a, fluxdensity_tot, T_star, R_star, dist_star, model = "thick"):
+    
+    
+    brightness = []
+    for i in range(len(limit)):
+        wavelength = wavelength_tot[limit[i][0]:limit[i][1]]
+        pre_factor = 1/(dist_star**2) * 2*c.h*c.c**2 /(wavelength**5) * np.pi
+        flare_contribution = (a*R_star)**2/(np.exp((c.h * c.c)/(T_flare*c.k*wavelength)) -1)
+        if model == "thick":
+            #total star in given passband #das ist super klein, deswegen hat es kaum einen Einfluss
+            star_contribution = -(a*R_star/dist_star)**2 * np.nansum(fluxdensity_tot[i]) * 1e7
+        else:
+            #thin case, a sign
+            star_contribution = (a*R_star)**2 * np.nansum(fluxdensity_tot[i]) * 1e7
+        brightness.append(np.nansum(pre_factor * (flare_contribution))  + star_contribution) 
+        
+    return np.asarray(brightness)
+
 
 def brightness_mod(wavelength_tot, limit, T_flare, a, fluxdensity_tot, T_star, R_star, dist_star, model = "thick"):
     
--- notebooks/funcs/probabilities_emcee.py
+++ notebooks/funcs/probabilities_emcee.py
@@ -1,10 +1,11 @@
 
 import numpy as np
-from funcs.blackbody_model import brightness_mod
+from funcs.blackbody_model import _brightness_mod
+from IPython.display import display, Math
 
 def log_likelihood(theta, x, y, yerr, limit, fluxdensity_star, T_star, R_star, dist_star):
     T, a, = theta
-    model = brightness_mod(x, limit, T, a , fluxdensity_star, T_star, R_star, dist_star) 
+    model = _brightness_mod(x, limit, T, a , fluxdensity_star, T_star, R_star, dist_star) 
     return -0.5 * (np.sum(((y - model)/yerr) ** 2 ))
                                                
 def log_prior_global_uniform(theta):
@@ -24,3 +25,51 @@ def log_probability(theta, x, y, yerr, limit, fluxdensity_star, T_star,R_star, d
     if not np.isfinite(lp):
         return -np.inf
     return lp + log_likelihood(theta, x, y, yerr, limit, fluxdensity_star, T_star, R_star, dist_star)
+    
+
+def plot_walker_emcee(samples):
+    
+    #Analysis Plot Walker
+   
+    labels = ["T", "a"]
+    for j in range(len(samples)):
+        fig, axes = plt.subplots(2, figsize=(10, 7), sharex=True)
+        for i in range(len(labels)):
+            
+            ax = axes[i]
+            ax.plot(samples_total[j][:, :, i], "k", alpha=0.3)
+            ax.set_xlim(0, len(samples_total[j]))
+            #ax.set_ylabel(labels[i])
+            ax.yaxis.set_label_coords(-0.1, 0.5)
+            axes[-1].set_xlabel("step number");
+    return 
+    
+def plot_corner_emcee(samples_flat):
+    labels = ["T","a"]
+    for j in range(len(samples_flat)):
+        fig = corner.corner(
+        samplesflat_total[j], labels=labels
+        );
+    return 
+
+def display_median_from_chain(samplesflat):
+    
+    T, Terror, a, aerror = [], [], [], []
+    
+    for j in range(len(samplesflat)):
+        for i in range(2):
+            mcmc = np.percentile(samplesflat[j][:, i], [16, 50, 84])
+            q = np.diff(mcmc)
+            txt = "\mathrm{{{3}}} = {0:.5f}_{{-{1:.5f}}}^{{{2:.5f}}}"
+            txt = txt.format(mcmc[1], q[0], q[1], labels[i])
+            display(Math(txt))
+            if i == 0:
+                T_med = mcmc[1]
+                T.append(T_med)
+                Terror.append(q)
+            if i == 1:
+                a_med = mcmc[1]
+                a.append(a_med) 
+                aerror.append(q) 
+                
+    return T, Terror, a, aerror  
